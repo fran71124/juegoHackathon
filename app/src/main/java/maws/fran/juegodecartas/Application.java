7c720b4431b4769837b6a9d02a3ceece375afc71
fran71124 <fran71124@gmail.com>
2017-09-29 17:34:38 +0200

    initial commit

---
+++ app/src/main/java/maws/fran/juegodecartas/Application.java
@@ -0,0 +1,8 @@
+package maws.fran.juegodecartas;
+
+/**
+ * Created by fran_ on 29/09/2017.
+ */
+
+public class Application extends android.app.Application {
+}
---
+++ app/src/main/java/maws/fran/juegodecartas/identification/Identification.java
@@ -0,0 +1,15 @@
+package maws.fran.juegodecartas.identification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import maws.fran.juegodecartas.R;
+
+public class Identification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identification);
+    }
+}
