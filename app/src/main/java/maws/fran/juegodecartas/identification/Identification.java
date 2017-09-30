94a9f9e97002d58aeeb527bd8f551bb95efc84d4
Catalín Damián <catalin.damian@cuatroochenta.com>
2017-09-30 10:19:34 +0200

    Firebase real

--- app/src/main/java/maws/fran/juegodecartas/identification/Identification.java
+++ app/src/main/java/maws/fran/juegodecartas/identification/Identification.java
@@ -1,18 +1,84 @@
 package maws.fran.juegodecartas.identification;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import maws.fran.juegodecartas.R;
 
 public class Identification extends AppCompatActivity {
 
+    private EditText mInputEmail;
+    private EditText mInputPassword;
+    private Button mButtonSignIn;
+    private Button mButtonSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identification);
-        Button button = (Button) findViewById(R.id.button);
 
+        initComponents();
+        initButtons();
+
+    }
+
+    private void initButtons() {
+        mButtonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(mInputEmail.getText().toString(), mInputPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(Identification.this, "Login success!!", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Identification.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+        mButtonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(mInputEmail.getText().toString(), mInputPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Identification.this, "User created!!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Identification.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
+    }
+
+    private void initComponents() {
+        mInputPassword = (EditText) findViewById(R.id.identification_input_password);
+        mButtonSignIn = (Button) findViewById(R.id.identification_button_sign_in);
+        mButtonSignUp = (Button) findViewById(R.id.identification_button_sign_up);
+        mInputEmail = (EditText) findViewById(R.id.identification_input_email);
     }
 }
